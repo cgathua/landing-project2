b142119a4bef1877778a48bdf6538933453dfce9
cgathua <114153640+cgathua@users.noreply.github.com>
2022-10-19 04:13:36 -0400

    feat: bento slider

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-const nav = document.querySelector('.nav-links');
-const openNavBtn = document.querySelector('#nav_toggle-open');
-const closeNavBtn = document.querySelector('#nav_toggle-close');
+(async () => {
+    const sidebar = document.querySelector('#sidebar1');
+    await customElements.whenDefined('bento-sidebar');
+    const api = await sidebar.getApi();
 
-const openNav = () => {
-    nav.style.display = 'flex';
-    openNavBtn.style.display = 'none';
-    closeNavBtn.style.display = 'inline-block'
-}
-
-openNavBtn.addEventListener('click', openNav);
-
-const closeNav = () => {
-    nav.style.display = 'none';
-    openNavBtn.style.display = 'inline-block';
-    closeNavBtn.style.display = 'none'
-}
-
-closeNavBtn.addEventListener('click', closeNav);
-
-nav.querySelectorAll('li a').forEach(navLink => {
-    navLink.addEventListener('click', closeNav);
-})
+    // set up button actions
+    document.querySelector('#open-sidebar').onclick = () => api.open();
+  })();
